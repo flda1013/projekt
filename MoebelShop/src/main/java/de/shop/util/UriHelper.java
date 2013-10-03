9527978a796362e73b091f53f3a5e00a424af18f
Daniel Fluegler <flda1013@hs-karlsruhe.de>
2013-10-03 22:55:53 +0200

    Neue Pom Get geht Post Put und Delet müssen noch geändert werden!

---
+++ MoebelShop/src/main/java/de/shop/util/UriHelper.java
@@ -0,0 +1,24 @@
+package de.shop.util;
+
+import java.net.URI;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.UriInfo;
+
+
+@ApplicationScoped
+@Log
+public class UriHelper {
+	public URI getUri(Class<?> clazz, UriInfo uriInfo) {
+		return uriInfo.getBaseUriBuilder()
+				      .path(clazz)
+				      .build();
+	}
+
+	public URI getUri(Class<?> clazz, String methodName, Long id, UriInfo uriInfo) {
+		return uriInfo.getBaseUriBuilder()
+		              .path(clazz)
+		              .path(clazz, methodName)
+		              .build(id);
+	}
+}
