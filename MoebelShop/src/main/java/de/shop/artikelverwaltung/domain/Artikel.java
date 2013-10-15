e457a90f332f9d7bed89c2720d1e836f59c165e1
Fabian <Fabian@user-th>
2013-10-15 22:01:04 +0200

    Versionsnummer + Anpassung ToString()

--- MoebelShop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
+++ MoebelShop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 import java.util.Date;
 
+import javax.persistence.Basic;
 import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -20,6 +21,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import javax.persistence.Version;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -75,12 +77,18 @@ public class Artikel implements Serializable {
 	public static final String PARAM_BEZEICHNUNG = "bezeichnung";
 	public static final String PARAM_PREIS = "preis";
 	public static final String PARAM_ID = "id";
+	
+	public static final int ERSTE_VERSION = 0;
 
 	@Id
 	@GeneratedValue
 	@Column(nullable = false, updatable = false)
 	@Min(value = MIN_ID, message = "{artikelverwaltung.artikel.id.min}", groups = IdGroup.class)
 	private Long id = KEINE_ID;
+	
+	@Version
+	@Basic(optional = false)
+	private int version = ERSTE_VERSION;
 	
 	@Column(length = BEZEICHNUNG_LENGTH_MAX, nullable = false)
 	@NotNull(message = "{artikelverwaltung.artikel.bezeichnung.notNull}")
@@ -235,10 +243,16 @@ public class Artikel implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Artikel [id=" + id + ", bezeichnung=" + bezeichnung
-		       + ", preis=" + preis + ", ausgesondert=" + ausgesondert
-		       + ", erzeugt=" + erzeugt
-			   + ", aktualisiert=" + aktualisiert + "]";
+		return "Artikel [id=" + id + ", version=" + version
+		       + ", bezeichnung=" + bezeichnung + ", ausgesondert=" + ausgesondert + "]";
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
 	}
 
 }
